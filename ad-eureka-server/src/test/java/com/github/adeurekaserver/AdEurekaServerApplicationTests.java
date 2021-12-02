fedf765115558abed291fa848defeb043d923325
jinrunheng <1175088275@qq.com>
2021-12-02 22:39:41 +0800

    配置单节点的 eureka server

---
+++ ad-eureka-server/src/test/java/com/github/adeurekaserver/AdEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
