d5716f116715d2c77ad0ed0f4429c4742c5f42b7
jinrunheng <1175088275@qq.com>
2021-12-08 10:34:33 +0800

    完成数据表实体类的定义——AdUnitIt,AdUnitKeyword,AdUnitDistrict

---
+++ ad-service/ad-sponsor/src/main/java/com/github/ad/entity/unit_condition/AdUnitDistrict.java
@@ -0,0 +1,42 @@
+package com.github.ad.entity.unit_condition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * 推广单元：
+ * <p>
+ * 地域限制
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "ad_unit_district")
+public class AdUnitDistrict {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "unit_id", nullable = false)
+    private Long unitId;
+
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    public AdUnitDistrict(Long unitId, String province, String city) {
+        this.unitId = unitId;
+        this.province = province;
+        this.city = city;
+    }
+}
---
+++ ad-service/ad-sponsor/src/main/java/com/github/ad/entity/unit_condition/AdUnitKeyword.java
@@ -0,0 +1,38 @@
+package com.github.ad.entity.unit_condition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * 推广单元：
+ * <p>
+ * 关键词限制
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "ad_unit_keyword")
+public class AdUnitKeyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "unit_id", nullable = false)
+    private Long unitId;
+
+    @Column(name = "keyword", nullable = false)
+    private String keyword;
+
+    public AdUnitKeyword(Long unitId, String keyword) {
+        this.unitId = unitId;
+        this.keyword = keyword;
+    }
+}
