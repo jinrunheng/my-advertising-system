a96581d30f4fd1c8c446f28e5a525ba1d37b1934
jinrunheng <1175088275@qq.com>
2021-12-09 14:18:38 +0800

    完成了推广单元限制 service 层的实现

--- ad-service/ad-sponsor/src/main/java/com/github/ad/service/impl/AdUnitServiceImpl.java
+++ ad-service/ad-sponsor/src/main/java/com/github/ad/service/impl/AdUnitServiceImpl.java
@@ -3,16 +3,33 @@ package com.github.ad.service.impl;
 import com.github.ad.constant.ErrorMsg;
 import com.github.ad.dao.AdPlanRepository;
 import com.github.ad.dao.AdUnitRepository;
+import com.github.ad.dao.unit_condition.AdUnitDistrictRepository;
+import com.github.ad.dao.unit_condition.AdUnitItRepository;
+import com.github.ad.dao.unit_condition.AdUnitKeywordRepository;
 import com.github.ad.entity.AdPlan;
 import com.github.ad.entity.AdUnit;
+import com.github.ad.entity.unit_condition.AdUnitDistrict;
+import com.github.ad.entity.unit_condition.AdUnitIt;
+import com.github.ad.entity.unit_condition.AdUnitKeyword;
 import com.github.ad.exception.AdException;
+import com.github.ad.request.AdUnitDistrictRequest;
+import com.github.ad.request.AdUnitItRequest;
+import com.github.ad.request.AdUnitKeywordRequest;
 import com.github.ad.request.AdUnitRequest;
+import com.github.ad.response.AdUnitDistrictResponse;
+import com.github.ad.response.AdUnitItResponse;
+import com.github.ad.response.AdUnitKeywordResponse;
 import com.github.ad.response.AdUnitResponse;
 import com.github.ad.service.AdUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AdUnitServiceImpl implements AdUnitService {
@@ -22,6 +39,15 @@ public class AdUnitServiceImpl implements AdUnitService {
 
     @Autowired
     private AdUnitRepository unitRepository;
+
+    @Autowired
+    private AdUnitKeywordRepository unitKeywordRepository;
+
+    @Autowired
+    private AdUnitItRepository unitItRepository;
+
+    @Autowired
+    private AdUnitDistrictRepository unitDistrictRepository;
 
     @Override
     public AdUnitResponse createUnit(AdUnitRequest request) throws AdException {
@@ -49,5 +75,81 @@ public class AdUnitServiceImpl implements AdUnitService {
                 .id(save.getId())
                 .unitName(save.getUnitName())
                 .build();
+    }
+
+    @Override
+    public AdUnitKeywordResponse createUnitKeyword(AdUnitKeywordRequest request) throws AdException {
+        List<Long> unitIds = request.getUnitKeywords()
+                .stream()
+                .map(AdUnitKeywordRequest.UnitKeyword::getUnitId)
+                .collect(Collectors.toList());
+
+        if (!isRelatedUnitExist(unitIds)) {
+            throw new AdException(ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        List<AdUnitKeyword> unitKeywords = new ArrayList<>();
+        request.getUnitKeywords().forEach(
+                unitKeyword -> unitKeywords.add(new AdUnitKeyword(unitKeyword.getUnitId(), unitKeyword.getKeyword()))
+        );
+
+        unitKeywordRepository.saveAll(unitKeywords);
+
+        return AdUnitKeywordResponse.builder()
+                .ids(unitIds)
+                .build();
+    }
+
+    @Override
+    public AdUnitItResponse createUnitIt(AdUnitItRequest request) throws AdException {
+        List<Long> unitIds = request.getUnitIts()
+                .stream()
+                .map(AdUnitItRequest.UnitIt::getUnitId)
+                .collect(Collectors.toList());
+
+        if (!isRelatedUnitExist(unitIds)) {
+            throw new AdException(ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        List<AdUnitIt> unitIts = new ArrayList<>();
+        request.getUnitIts().forEach(
+                unitIt -> unitIts.add(new AdUnitIt(unitIt.getUnitId(), unitIt.getItTag()))
+        );
+
+        unitItRepository.saveAll(unitIts);
+
+        return AdUnitItResponse.builder()
+                .ids(unitIds)
+                .build();
+    }
+
+    @Override
+    public AdUnitDistrictResponse createUnitDistrict(AdUnitDistrictRequest request) throws AdException {
+        List<Long> unitIds = request.getUnitDistricts()
+                .stream()
+                .map(AdUnitDistrictRequest.UnitDistrict::getUnitId)
+                .collect(Collectors.toList());
+
+        if (!isRelatedUnitExist(unitIds)) {
+            throw new AdException(ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        List<AdUnitDistrict> unitDistricts = new ArrayList<>();
+        request.getUnitDistricts().forEach(
+                unitDistrict -> unitDistricts.add(
+                        new AdUnitDistrict(unitDistrict.getUnitId(), unitDistrict.getProvince(), unitDistrict.getCity()))
+        );
+
+        unitDistrictRepository.saveAll(unitDistricts);
+        return AdUnitDistrictResponse.builder()
+                .ids(unitIds)
+                .build();
+    }
+
+    private boolean isRelatedUnitExist(List<Long> unitIds) {
+        if (CollectionUtils.isEmpty(unitIds)) {
+            return false;
+        }
+        return unitRepository.findAllById(unitIds).size() == new HashSet<>(unitIds).size();
     }
 }
