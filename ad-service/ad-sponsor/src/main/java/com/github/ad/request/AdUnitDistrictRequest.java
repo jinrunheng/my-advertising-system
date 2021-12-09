3863c43ae8cbeaa9963bfc6f0b2239eca9d60036
jinrunheng <1175088275@qq.com>
2021-12-09 11:40:36 +0800

    定义了推广单元限制（It,District,Keyword）的 VO（request,response）

---
+++ ad-service/ad-sponsor/src/main/java/com/github/ad/request/AdUnitDistrictRequest.java
@@ -0,0 +1,24 @@
+package com.github.ad.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitDistrictRequest {
+
+    private List<UnitDistrict> unitDistricts;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UnitDistrict {
+        private Long unitId;
+        private String province;
+        private String city;
+    }
+}
--- ad-service/ad-sponsor/src/main/java/com/github/ad/service/AdUnitService.java
+++ ad-service/ad-sponsor/src/main/java/com/github/ad/service/AdUnitService.java
@@ -1,9 +1,21 @@
 package com.github.ad.service;
 
 import com.github.ad.exception.AdException;
+import com.github.ad.request.AdUnitDistrictRequest;
+import com.github.ad.request.AdUnitItRequest;
+import com.github.ad.request.AdUnitKeywordRequest;
 import com.github.ad.request.AdUnitRequest;
+import com.github.ad.response.AdUnitDistrictResponse;
+import com.github.ad.response.AdUnitItResponse;
+import com.github.ad.response.AdUnitKeywordResponse;
 import com.github.ad.response.AdUnitResponse;
 
 public interface AdUnitService {
     AdUnitResponse createUnit(AdUnitRequest request) throws AdException;
+
+    AdUnitKeywordResponse createUnitKeyword(AdUnitKeywordRequest request) throws AdException;
+
+    AdUnitItResponse createUnitIt(AdUnitItRequest request) throws AdException;
+
+    AdUnitDistrictResponse createUnitDistrict(AdUnitDistrictRequest request) throws AdException;
 }
